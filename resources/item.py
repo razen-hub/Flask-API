57e950e3bfe7adfdd28bca76b15709ae7daacaea
Marwane Lotfi <marwane.lotfi@gmail.com>
2024-12-18 16:31:01 +0100

    FIRST COMMIT#

---
+++ resources/item.py
@@ -0,0 +1,57 @@
+from sqlalchemy.exc import SQLAlchemyError
+
+from db import db
+from models import ItemModel
+
+import uuid
+from flask_smorest import Blueprint,abort
+from flask import request
+from flask.views import MethodView
+
+from schemas import ItemSchema, ItemUpdateSchema
+
+
+blp = Blueprint("Items",__name__,description="Operations on items")
+
+
+@blp.route('/item/<string:item_id>')
+class Item(MethodView):
+    @blp.response(200)
+    def get(self, item_id):
+       item = ItemModel.query.get_or_404(item_id)
+       return item
+       
+       
+    def delete(self, item_id):
+       item = ItemModel.query.get_or_404(item_id)
+       db.session.delete(item)
+       db.session.commit()
+       return {"message":"Item deleted"}
+    
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200,ItemSchema)
+    def put(self,item_data,item_id):
+        item = ItemModel.query.get_or_404(item_id)
+        raise NotImplementedError("Deleting an item is not implemented ")
+   
+            
+@blp.route("/item")
+class ItemList(MethodView):
+    
+    @blp.response(200, ItemSchema(many=True))
+    def get(self):
+        return ItemModel.query.all()
+
+    @blp.arguments(ItemSchema)
+    @blp.response(201,ItemSchema)
+    def post(self,item_data):
+        item = ItemModel(**item_data)#keyword arguments
+        
+        try:
+            db.session.add(item)
+            db.session.commit()
+        except SQLAlchemyError:
+            abort(500,message="An error occured while inserting the item.")
+        
+        return item
+    
